de7cd23d61827f3e301885057ebb1e19a93efbf4
Dejonckheere Ward <ward.dejonckheere@student.howest.be>
2022-12-13 17:48:29 +0100

    Removed DAE namespace

--- source/Renderer.h
+++ source/Renderer.h
@@ -1,50 +1,49 @@
 #pragma once
 
+using namespace dae;
+
 struct SDL_Window;
 struct SDL_Surface;
 class Mesh;
 
-namespace dae
+class Camera;
+class Renderer final
 {
-	class Camera;
-	class Renderer final
-	{
-	public:
-		Renderer(SDL_Window* pWindow);
-		~Renderer();
+public:
+	Renderer(SDL_Window* pWindow);
+	~Renderer();
 
-		Renderer(const Renderer&) = delete;
-		Renderer(Renderer&&) noexcept = delete;
-		Renderer& operator=(const Renderer&) = delete;
-		Renderer& operator=(Renderer&&) noexcept = delete;
+	Renderer(const Renderer&) = delete;
+	Renderer(Renderer&&) noexcept = delete;
+	Renderer& operator=(const Renderer&) = delete;
+	Renderer& operator=(Renderer&&) noexcept = delete;
 
-		void Update(const Timer* pTimer);
-		void Render() const;
+	void Update(const Timer* pTimer);
+	void Render() const;
 
-	private:
-		SDL_Window* m_pWindow{};
+private:
+	SDL_Window* m_pWindow{};
 
-		int m_Width{};
-		int m_Height{};
+	int m_Width{};
+	int m_Height{};
 
-		bool m_IsInitialized{ false };
+	bool m_IsInitialized{ false };
 
-		//DIRECTX
-		HRESULT InitializeDirectX();
+	//DIRECTX
+	HRESULT InitializeDirectX();
 
-		ID3D11Device* m_pDevice;
-		ID3D11DeviceContext* m_pDeviceContext;
+	ID3D11Device* m_pDevice;
+	ID3D11DeviceContext* m_pDeviceContext;
 
-		IDXGISwapChain* m_pSwapChain;
+	IDXGISwapChain* m_pSwapChain;
 
-		ID3D11Texture2D* m_pDepthStencilBuffer;
-		ID3D11DepthStencilView* m_pDepthStencilView;
+	ID3D11Texture2D* m_pDepthStencilBuffer;
+	ID3D11DepthStencilView* m_pDepthStencilView;
 
-		ID3D11Texture2D* m_pRenderTargetBuffer;
-		ID3D11RenderTargetView* m_pRenderTargetView;
+	ID3D11Texture2D* m_pRenderTargetBuffer;
+	ID3D11RenderTargetView* m_pRenderTargetView;
 
-		Mesh* m_pMesh;
-		Camera* m_pCamera;
+	Mesh* m_pMesh;
+	Camera* m_pCamera;
 
-	};
-}
+};
